4e4b4cad4fdaeef2b57fdaac23029b937596bc93
Myan Poonian <myansingh1@gmail.com>
2025-10-08 11:03:17 +0100

    Sweeping radar visuals

--- display/src/main/java/FormatRadar.java
+++ display/src/main/java/FormatRadar.java
@@ -1,14 +1,19 @@
 package display;
 
+import java.lang.Math;
+
 import javafx.collections.ObservableList; 
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.animation.AnimationTimer;
@@ -18,7 +23,9 @@ import display.KeyPage;
 import display.DisplayObject;
 
 public class FormatRadar extends Format{
-  
+  private double currentAngle = 0;
+  private long lastNow = 0;
+  private long deltaTime = 0;
   Text blankText = new Text();
   
   public FormatRadar(MHDD parent) {
@@ -30,11 +37,51 @@ public class FormatRadar extends Format{
     //Set Page 0 as active page
     keyPages[0].select();
     
-    //Create example text on display
-    blankText = new Text(home.getX() + 30, home.getY() + 157, "RADAR FORMAT");
-    blankText.setFont(new Font(30));
-    blankText.setFill(Color.WHITE);
-    groupChildren.add(blankText);    
+    //Create radar display
+    Point3D circleCentre = new Point3D((home.getX() + CNST.SCREEN_SIZE / 2), (home.getY() + CNST.SCREEN_SIZE / 2), 0);
+    Circle radarCircle = new Circle(circleCentre.getX(), circleCentre.getY(), 120);
+    radarCircle.setFill(Color.TRANSPARENT);
+    radarCircle.setStroke(Color.GREEN);
+    radarCircle.setStrokeWidth(2);
+    groupChildren.add(radarCircle);     
+    
+    double radius = 120;
+    double radarRotationSpeed = Math.toRadians(180);
+
+    //Create radar line
+    Line radarLine = new Line();
+    radarLine.setStartX(circleCentre.getX());
+    radarLine.setStartY(circleCentre.getY());
+    radarLine.setStroke(Color.WHITE);
+    radarLine.setStrokeWidth(2);
+    groupChildren.add(radarLine);
+
+    //Rotate radarLine about centre
+    AnimationTimer rotationTimer = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+            deltaTime = now - lastNow;
+            
+            if (deltaTime > 0) {
+                deltaTime = Math.min(deltaTime, CNST.DELTA_TIME_CAP);
+              
+                double deltaSeconds = deltaTime / 1_000_000_000.0;
+                currentAngle += radarRotationSpeed * deltaSeconds;
+                
+                if (currentAngle > Math.PI * 2) {
+                    currentAngle -= Math.PI * 2;
+                }
+                
+                double x = circleCentre.getX() + radius * Math.cos(currentAngle);
+                double y = circleCentre.getY() + radius * Math.sin(currentAngle);
+                
+                radarLine.setEndX(x);
+                radarLine.setEndY(y);
+            }            
+            lastNow = now;
+        }
+    };
+    rotationTimer.start();
   }
   
   private void setUpKeys() {
