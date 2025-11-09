b209bd1a43b2dddcf5a254da201b1e2e6bf81fb0
bfc-raghav <30250525@blackpool.ac.uk>
2025-11-09 18:11:08 +0000

    Added zoom functionality

--- display/src/main/java/FormatNav.java
+++ display/src/main/java/FormatNav.java
@@ -11,6 +11,9 @@ import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.transform.Scale;
+import javafx.scene.input.MouseEvent;
+import javafx.event.EventHandler;
 
 public class FormatNav extends Format {
   private Image mapImage;
@@ -19,14 +22,19 @@ public class FormatNav extends Format {
   private Rectangle clipRect;     // clip for the minimap square
   private Polygon aircraftMarker; // fixed in the centre of the minimap
 
-  private final double MAP_SCALE = 4.0; // world space -> image space (pixels)
+  private double MAP_SCALE = 1.0; // world space -> image space (pixels)
+  private final double MAP_SCALE_MIN = 0.25; 
+  private final double MAP_SCALE_MAX = 4.0; 
+  private final double MAP_SCALE_STEP = 0.25; 
+
+  
   private AircraftData aircraftData;
 
   public FormatNav(MHDD parent) {
     this.parent = parent;
     this.home = parent.home;
     this.aircraftData = parent.parent.aircraftData;
-
+    
     setUpKeys();
     keyPages[0].select();
 
@@ -67,26 +75,70 @@ public class FormatNav extends Format {
   }
 
   private void update() {
+    mapView.getTransforms().clear();
+    
     Point3D pos = aircraftData.getPos(); // world coords (metres)
     double heading = -aircraftData.getHeading(); // degrees (dlipped)
-
+    
     //Convert coordinates from world space to pixel space
-    double imgCenterX = pos.getX() * MAP_SCALE;
-    double imgCenterY = -(pos.getY() * MAP_SCALE);
+    double imgCentreX = pos.getX() * MAP_SCALE;
+    double imgCentreY = -(pos.getY() * MAP_SCALE);
+
+    Scale scale = new Scale(MAP_SCALE, MAP_SCALE, imgCentreX, imgCentreY);
+    mapView.getTransforms().add(scale);
+
     // Translate ImageView to reflect aicraft pos
     double halfScreen = CNST.SCREEN_SIZE / 2.0;
-    double translateX = -imgCenterX + halfScreen;
-    double translateY = -imgCenterY + halfScreen;
+    double translateX = -imgCentreX + halfScreen;
+    double translateY = -imgCentreY + halfScreen;
     mapView.setTranslateX(translateX);
     mapView.setTranslateY(translateY);
 
     // Remove any previous Rotate transforms and apply rotation
-    mapView.getTransforms().removeIf(t -> t instanceof javafx.scene.transform.Rotate);
-    javafx.scene.transform.Rotate mapRotate = new javafx.scene.transform.Rotate(heading, imgCenterX, imgCenterY);
+    javafx.scene.transform.Rotate mapRotate = new javafx.scene.transform.Rotate(heading, imgCentreX, imgCentreY);
     mapView.getTransforms().add(mapRotate);
   }
+
+  private void zoomIn()  { if (MAP_SCALE < MAP_SCALE_MAX) { MAP_SCALE += MAP_SCALE_STEP; }  }
+  private void zoomOut() { if (MAP_SCALE > MAP_SCALE_MIN) { MAP_SCALE -= MAP_SCALE_STEP; }  }
   
   private void setUpKeys() {
     setUpFormatMenu();
+    
+    // Create zoom control key page
+    keyPages[2] = new KeyPage(this, "ZOOM    ", "ZOOM IN ", "ZOOM OUT", CNST.BLANK_LEGEND);
+    
+    // Zoom In button
+    EventHandler zoomInAction = new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event) {
+        zoomIn();
+      }
+    };
+    keyPages[2].keys[1].setAction(zoomInAction);
+    
+    // Zoom Out button
+    EventHandler zoomOutAction = new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event) {
+        zoomOut();
+      }
+    };
+    keyPages[2].keys[2].setAction(zoomOutAction);
+    
+    // Add ZOOM button to main menu
+    keyPages[0].keys[1].setLegend("ZOOM    ");
+    EventHandler showZoomMenu = new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event) {
+        selectPage(2);
+      }
+    };
+    keyPages[0].keys[1].setAction(showZoomMenu);
+    
+    // Back button on zoom menu
+    EventHandler hideZoomMenu = new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event) {
+        selectPage(0);
+      }
+    };
+    keyPages[2].keys[0].setAction(hideZoomMenu);
   }
 }
